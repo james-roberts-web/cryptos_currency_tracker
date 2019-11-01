bcba94bf0eab2b7a38583bf0362e94e795471494
Calum Chisholm <calum.chisholm@hotmail.com>
2019-11-01 12:30:34 +0000

    folder sturcture and back end data complete

---
+++ crypto_app/server/db/seeds.js
@@ -0,0 +1,24 @@
+use accounts;
+db.dropDatabase();
+db.user.insertMany([
+  {
+    name: "Sandra",
+    wallet_value: "1200",
+    portfolio: ["Bitcoin", "Etherium"]
+  },
+  {
+    name: "John",
+    wallet_value: "5000",
+    portfolio: ["Ripple", "Dogecoin", "Xero", "Nano", "Dash", "Mithril", "Bitcoin"]
+  },
+  {
+    name: "Kelly",
+    wallet_value: "100",
+    portfolio: ["Bitcoin", "Monero"]
+  },
+  {
+    name: "Frank",
+    wallet_value: "300",
+    portfolio: ["Bitcoin"]
+  }
+]);
