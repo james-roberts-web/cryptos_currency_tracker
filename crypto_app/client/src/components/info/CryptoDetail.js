acd01341064d93883225bf06ba0f093e74518b48
denkoroku <cathriddoch@gmail.com>
2019-11-04 16:05:15 +0000

    adds table and formats numbers

--- crypto_app/client/src/components/info/CryptoDetail.js
+++ crypto_app/client/src/components/info/CryptoDetail.js
@@ -6,15 +6,73 @@ const CryptoDetail = (props) => {
     <div className="details">
     <img src={props.currency.image} alt = "currency logo"></img>
       <h3>{ props.currency.name } </h3>
-      <p>Abbreviation: {props.currency.symbol}</p>
-      <p>Current Price: £{props.currency.current_price}</p>
-      <p>Highest value in last 24 hours: £{props.currency.high_24h}</p>
-      <p>Lowest value in last 24 hours: £{props.currency.low_24h}</p>
-      <p>Percent change in last 24 hours: {props.currency.price_change_percentage_24h}%</p>
-      <p>Percent change in last hour: {props.currency.price_change_percentage_1h_in_currency}%</p>
-      <p>Market cap: £{props.currency.market_cap}</p>
-      <p>Total Supply: £{props.currency.total_supply}</p>
-      <p>Total Volume: £{props.currency.total_volume}</p>
+      <h4>{props.currency.symbol}</h4>
+      <table>
+        <tr>
+          <td>
+            Current Price:
+          </td>
+          <td>
+            £{props.currency.current_price}
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Highest value in last 24 hours:
+          </td>
+          <td>
+            £{props.currency.high_24h}
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Lowest value in last 24 hours:
+          </td>
+          <td>
+            £{props.currency.low_24h}
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Percent change in last 24 hours:
+          </td>
+          <td>
+            {props.currency.price_change_percentage_24h.toPrecision(4)}%
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Percent change in last hour:
+          </td>
+          <td>
+            {props.currency.price_change_percentage_1h_in_currency.toPrecision(4)}%
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Market cap:
+          </td>
+          <td>
+            £{props.currency.market_cap}
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Total Supply:
+          </td>
+          <td>
+            £{props.currency.total_supply}
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Total Volume:
+          </td>
+          <td>
+            £{props.currency.total_volume}
+          </td>
+        </tr>
+      </table>
 
     </div>
   )
